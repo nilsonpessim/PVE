d3171c3cd252901613a1a1a43b4498cf6dfc8155
Nilson Pessim <nilson@techlabs.net.br>
2022-07-06 20:55:00 -0300

    Create Config-Example.php

---
+++ source/Config/Config-Example.php
@@ -0,0 +1,9 @@
+<?php
+
+define("CONF_PVE_HOST", "127.0.0.1");
+define("CONF_PVE_USER", "root");
+define("CONF_PVE_PASS", "");
+define("CONF_PVE_REALM", "pam");
+define("CONF_PVE_PORT", 8006);
+
+define("CONF_DEFAULT_METHODS_GET", "/nodes");
